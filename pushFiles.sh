b0c0dd353a60fe81559b65d8263a009afb5d2b5f
kschwank <karsten.schwank@gmail.com>
2013-03-18 14:18:58 +0100

    add template file functionality to use an existing file as template for created files.

--- pushFiles.sh
+++ pushFiles.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
+if [ $# -lt 2 ]
 then
-    echo "Usage: pushFiles.sh <file count> <directory>"
+    echo "Usage: pushFiles.sh <file count> <directory> [<template file>]"
     exit 1
 fi
 
-echo "creating $1 small files in $2"
-i=1
+echo "creating $1 files in $2"
 for i in $(seq $1)
 do
-    echo $i > $(mktemp --dry-run --tmpdir="${2}" sfXXXXXXXXXX)
+    filename=$(mktemp --dry-run --tmpdir="${2}" sfXXXXXXXXXX)
+    if [ -z ${3} ]
+    then
+        echo $i > "${filename}"
+    else
+        cp "${3}" "${filename}"
+    fi
 done
 
 echo "done."
