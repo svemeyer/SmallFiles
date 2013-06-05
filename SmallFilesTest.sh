dc7af57886a76f7696bbac926f3a9b6e20970eaa
Karsten Schwank <karsten.schwank@desy.de>
2013-06-05 16:19:20 +0200

    Adjust test to use 40k files and pack 200 files per archive

--- SmallFilesTest.sh
+++ SmallFilesTest.sh
@@ -2,16 +2,14 @@
 
 testDir="tests"
 sName="test"
-testfile="/root/testfile"
-fcount=60
+testfile="/root/40k"
+fcount=2000
+fdelay=""
 
-echo "Cleaning test directories"
-rm -rf "/pnfs/4/${testDir}"
-rm -rf "/pnfs/4/hsm/requests/${sName}"
-rm -rf "/pnfs/4/hsm/archives/${sName}"
+sh clearDirs.sh
 
 echo "Cleaning crontab"
-sed -i 's%^\*  \*  \*  \*  \* root /usr/share/dcache/lib/pack-files.sh \"/data\" \"/pnfs/4\" \"hsm\" 200000000 2>&1$%%' /etc/crontab
+sed -i 's%^\*  \*  \*  \*  \* root /usr/share/dcache/lib/pack-files.sh \"/data\" \"/pnfs/4\" \"hsm\" .*$%%' /etc/crontab
 
 echo "Creating test directories"
 
@@ -36,17 +34,17 @@ echo "smallfiles" > "/pnfs/4/${testDir}/user/exp3/.(tag)(sGroup)"
 mkdir -p "/pnfs/4/${testDir}/user/exp3/sub1"
 mkdir -p "/pnfs/4/${testDir}/user/exp3/sub2"
 
-echo "Creating files in test directories"
-pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp1" "${testfile}"
-pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp1/sub1" "${testfile}"
-pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp1/sub2" "${testfile}"
-pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp2" "${testfile}"
-pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp2/sub1" "${testfile}"
-pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp2/sub2" "${testfile}"
-pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp3" "${testfile}"
-pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp3/sub1" "${testfile}"
-pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp3/sub2" "${testfile}"
-
 echo "Creating crontab entry"
-echo '*  *  *  *  * root /usr/share/dcache/lib/pack-files.sh "/data" "/pnfs/4" "hsm" 200000000 2>&1' >> /etc/crontab
+echo '*  *  *  *  * root /usr/share/dcache/lib/pack-files.sh "/data" "/pnfs/4" "hsm" 8000000 2>&1' >> /etc/crontab
+
+echo "Creating files in test directories"
+pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp1" "${testfile}" "${fdelay}"
+pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp2" "${testfile}" "${fdelay}"
+pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp3" "${testfile}" "${fdelay}"
+pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp1/sub1" "${testfile}" "${fdelay}"
+pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp1/sub2" "${testfile}" "${fdelay}"
+pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp2/sub1" "${testfile}" "${fdelay}"
+pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp2/sub2" "${testfile}" "${fdelay}"
+pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp3/sub1" "${testfile}" "${fdelay}"
+pushFiles.sh ${fcount} "/pnfs/4/${testDir}/user/exp3/sub2" "${testfile}" "${fdelay}"
 
