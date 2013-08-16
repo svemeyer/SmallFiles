13481480b41c0d29fdcae59f632221aa82b72039
Karsten Schwank <karsten.schwank@desy.de>
2013-08-16 15:22:31 +0200

    use loops to create test directories

--- skel/usr/local/bin/SmallFilesTest.sh
+++ skel/usr/local/bin/SmallFilesTest.sh
@@ -5,9 +5,10 @@ testDir="tests"
 sName="test"
 testfile="40k"
 fcount=2000
+dirCount=3
+subDirCount=2
 fdelay=""
 
-echo "Cleaning ${targetRoot}/${testDir} ..."
 sh clearDirs.sh
 
 echo "Cleaning crontab"
@@ -15,38 +16,26 @@ sed -i "s%^\*  \*  \*  \*  \* root .*/pack-files.sh .*$%%" /etc/crontab
 
 echo "Creating test directories"
 
-mkdir -p "${targetRoot}/${testDir}/user/exp1"
-echo "dcache" > "${targetRoot}/${testDir}/user/exp1/.(tag)(hsmInstance)"
-echo "StoreName ${sName}" > "${targetRoot}/${testDir}/user/exp1/.(tag)(OSMTemplate)"
-echo "smallfiles" > "${targetRoot}/${testDir}/user/exp1/.(tag)(sGroup)"
-mkdir -p "${targetRoot}/${testDir}/user/exp1/sub1"
-mkdir -p "${targetRoot}/${testDir}/user/exp1/sub2"
+for exp in $(seq 1 $dirCount); do
+  mkdir -p "${targetRoot}/${testDir}/user/exp$exp"
+  echo "dcache" > "${targetRoot}/${testDir}/user/exp$exp/.(tag)(hsmInstance)"
+  echo "StoreName ${sName}" > "${targetRoot}/${testDir}/user/exp$exp/.(tag)(OSMTemplate)"
+  echo "smallfiles" > "${targetRoot}/${testDir}/user/exp$exp/.(tag)(sGroup)"
 
-mkdir -p "${targetRoot}/${testDir}/user/exp2"
-echo "dcache" > "${targetRoot}/${testDir}/user/exp2/.(tag)(hsmInstance)"
-echo "StoreName ${sName}" > "${targetRoot}/${testDir}/user/exp2/.(tag)(OSMTemplate)"
-echo "smallfiles" > "${targetRoot}/${testDir}/user/exp2/.(tag)(sGroup)"
-mkdir -p "${targetRoot}/${testDir}/user/exp2/sub1"
-mkdir -p "${targetRoot}/${testDir}/user/exp2/sub2"
-
-mkdir -p "${targetRoot}/${testDir}/user/exp3"
-echo "dcache" > "${targetRoot}/${testDir}/user/exp3/.(tag)(hsmInstance)"
-echo "StoreName ${sName}" > "${targetRoot}/${testDir}/user/exp3/.(tag)(OSMTemplate)"
-echo "smallfiles" > "${targetRoot}/${testDir}/user/exp3/.(tag)(sGroup)"
-mkdir -p "${targetRoot}/${testDir}/user/exp3/sub1"
-mkdir -p "${targetRoot}/${testDir}/user/exp3/sub2"
+  for sub in $(seq 1 $subDirCount); do
+    mkdir -p "${targetRoot}/${testDir}/user/exp$exp/sub$sub"
+    mkdir -p "${targetRoot}/${testDir}/user/exp$exp/sub$sub"
+  done
+done
 
 echo "Creating crontab entry"
 echo "*  *  *  *  * root /usr/local/bin/pack-files.sh \"/data\" \"${targetRoot}\" \"hsm\" 8000000 2>&1" >> /etc/crontab
 
 echo "Creating files in test directories"
-pushFiles.sh ${fcount} "${targetRoot}/${testDir}/user/exp1" "${testfile}" "${fdelay}"
-pushFiles.sh ${fcount} "${targetRoot}/${testDir}/user/exp2" "${testfile}" "${fdelay}"
-pushFiles.sh ${fcount} "${targetRoot}/${testDir}/user/exp3" "${testfile}" "${fdelay}"
-pushFiles.sh ${fcount} "${targetRoot}/${testDir}/user/exp1/sub1" "${testfile}" "${fdelay}"
-pushFiles.sh ${fcount} "${targetRoot}/${testDir}/user/exp1/sub2" "${testfile}" "${fdelay}"
-pushFiles.sh ${fcount} "${targetRoot}/${testDir}/user/exp2/sub1" "${testfile}" "${fdelay}"
-pushFiles.sh ${fcount} "${targetRoot}/${testDir}/user/exp2/sub2" "${testfile}" "${fdelay}"
-pushFiles.sh ${fcount} "${targetRoot}/${testDir}/user/exp3/sub1" "${testfile}" "${fdelay}"
-pushFiles.sh ${fcount} "${targetRoot}/${testDir}/user/exp3/sub2" "${testfile}" "${fdelay}"
 
+for exp in $(seq 1 $dirCount); do
+  pushFiles.sh ${fcount} "${targetRoot}/${testDir}/user/exp$exp" "${testfile}" "${fdelay}"
+  for sub in $(seq 1 $subDirCount); do
+    pushFiles.sh ${fcount} "${targetRoot}/${testDir}/user/exp$exp/sub$sub" "${testfile}" "${fdelay}"
+  done
+done
