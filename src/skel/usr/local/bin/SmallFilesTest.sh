f6c9afaf95eb881fd8d14b0f956e3b5998ec92d1
Karsten Schwank <karsten.schwank@desy.de>
2013-08-21 08:13:42 +0200

    SmallFilesTest.sh: improve variable naming

--- src/skel/usr/local/bin/SmallFilesTest.sh
+++ src/skel/usr/local/bin/SmallFilesTest.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 packFilesScript=`which pack-files.sh`
-targetRoot=`mount|grep "minorversion=1,vers=4"|awk '{ print $3 }'`
-remoteExport=`mount|grep "minorversion=1,vers=4"|awk '{ print $1 }'|sed 's/[^:].*://'`
+nfs41MountPoint=`mount|grep "minorversion=1,vers=4"|awk '{ print $3 }'`
+nfs41Export=`mount|grep "minorversion=1,vers=4"|awk '{ print $1 }'|sed 's/[^:].*://'`
 testDir="tests"
 sName="test"
-testfile="1M"
-filesize=1000000
-fcount=1000
 dirCount=3
-subDirCount=2
-archiveSize=$(( $filesize * 1000 ))
+subDirCount=0
+testFile="1k"
+fileSize=1000
+filesPerDir=30000
+filesPerArchive=100
+archiveSize=$(( $fileSize * $filesPerArchive ))
 fdelay=""
 
 sh clearDirs.sh
@@ -21,25 +22,25 @@ sed -i "s%^\*  \*  \*  \*  \* root .*/pack-files.sh .*$%%" /etc/crontab
 echo "Creating test directories"
 
 for exp in $(seq 1 $dirCount); do
-  mkdir -p "${targetRoot}/${testDir}/user/exp$exp"
-  echo "dcache" > "${targetRoot}/${testDir}/user/exp$exp/.(tag)(hsmInstance)"
-  echo "StoreName ${sName}" > "${targetRoot}/${testDir}/user/exp$exp/.(tag)(OSMTemplate)"
-  echo "smallfiles" > "${targetRoot}/${testDir}/user/exp$exp/.(tag)(sGroup)"
+  mkdir -p "${nfs41MountPoint}/${testDir}/user/exp$exp"
+  echo "dcache" > "${nfs41MountPoint}/${testDir}/user/exp$exp/.(tag)(hsmInstance)"
+  echo "StoreName ${sName}" > "${nfs41MountPoint}/${testDir}/user/exp$exp/.(tag)(OSMTemplate)"
+  echo "smallfiles" > "${nfs41MountPoint}/${testDir}/user/exp$exp/.(tag)(sGroup)"
 
   for sub in $(seq 1 $subDirCount); do
-    mkdir -p "${targetRoot}/${testDir}/user/exp$exp/sub$sub"
-    mkdir -p "${targetRoot}/${testDir}/user/exp$exp/sub$sub"
+    mkdir -p "${nfs41MountPoint}/${testDir}/user/exp$exp/sub$sub"
+    mkdir -p "${nfs41MountPoint}/${testDir}/user/exp$exp/sub$sub"
   done
 done
 
 echo "Creating crontab entry"
-echo "*  *  *  *  * root ${packFilesScript} \"${remoteExport}\" \"${targetRoot}\" \"hsm\" ${archiveSize} 2>&1" >> /etc/crontab
+echo "*  *  *  *  * root ${packFilesScript} \"${nfs41Export}\" \"${nfs41MountPoint}\" \"hsm\" ${archiveSize} 2>&1" >> /etc/crontab
 
 echo "Creating files in test directories"
 
 for exp in $(seq 1 $dirCount); do
-  pushFiles.sh ${fcount} "${targetRoot}/${testDir}/user/exp$exp" "${testfile}" "${fdelay}"
+  pushFiles.sh ${filesPerDir} "${nfs41MountPoint}/${testDir}/user/exp$exp" "${testFile}" "${fdelay}"
   for sub in $(seq 1 $subDirCount); do
-    pushFiles.sh ${fcount} "${targetRoot}/${testDir}/user/exp$exp/sub$sub" "${testfile}" "${fdelay}"
+    pushFiles.sh ${filesPerDir} "${nfs41MountPoint}/${testDir}/user/exp$exp/sub$sub" "${testFile}" "${fdelay}"
   done
 done
