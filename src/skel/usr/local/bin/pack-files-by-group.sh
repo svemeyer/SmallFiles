79f720b9eb8dc96ec232ba45f5864fb5930bbbea
Karsten Schwank <karsten.schwank@desy.de>
2013-10-21 09:08:50 +0200

    add extra script for packing files by group rather than by directory

---
+++ src/skel/usr/local/bin/pack-files-by-group.sh
@@ -0,0 +1,334 @@
+#!/bin/bash
+#
+set -x
+# $Id: pack-files.sh 2013-02-11 karsten
+#
+# This script should be run manually or as a cron job. It is part of a set of 
+# scripts that will collect, pack, and restore files in the context of dCache 
+# (www.dcache.org) to improve the performance when handling many small files. 
+#
+# This scipt uses several special directories that are descibed in more detail 
+# below. In the directory "flagDir" it expects empty files, named like the pnfsids
+# of the files to be packed (this is done by the script hsm-internal.sh). 
+# This script (pack-files.sh) will take those empty files' names, and create a 
+# temporary directory containing a collection of symbolic links to the corresponding
+# (small) files. If this collection exceeds some configured size (default 1gb) 
+# the files are packed in an archive file in $hsmDir. Afterwards the files
+# in $flagdir are filled with a special uri that contains the file's and the 
+# archive's pnfsids. This uri is then picked up and stored with the file in chimera 
+# by hsm-internal.sh. 
+#
+# Parameters: 
+#
+#   $chimeraDataPrefix:
+#             This is the nfs exported directory as configured in /etc/exports.
+#             E.g., "/data"
+#
+#   $mountPoint:
+#             This is the mount point where dCache is mounted with nfs 4.1
+#             E.g., "/pnfs/4"
+#
+#   $hsmBase:
+#             This is base directory that will be used by the scripts to perform
+#             their task relative to $mountPoint respectively $dcachePrefix. Below this
+#             directory there have to be two sub directories: "archives" and "requests".
+#             "archives" has to be tagged with hsmInstance "osm" and "CUSTODIAL"/"NEARLINE"
+#             and "requests" has to be configured to be "REPLICA"/"ONLINE".
+#             (i.e., 
+#               # chimera-cli writetag "${dcachePrefix}/${hsmBase}/requests" AccessLatency ONLINE
+#               # chimera-cli writetag "${dcachePrefix}/${hsmBase}/requests" RetentionPolicy REPLICA
+#               # chimera-cli writetag "${dcachePrefix}/${hsmBase}/archives" AccessLatency NEARLINE
+#               # chimera-cli writetag "${dcachePrefix}/${hsmBase}/archives" RetentionPolicy CUSTODIAL
+#               OR
+#               # echo "ONLINE" > "${mountPoint}/${hsmBase}/requests/.(tag)(AccessLatency)"
+#               # echo "REPLICA" > "${mountPoint}/${hsmBase}/requests/.(tag)(RetentionPolicy)"
+#               # echo "NEARLINE" > "${mountPoint}/${hsmBase}/archives/.(tag)(AccessLatency)"
+#               # echo "CUSTODIAL" > "${mountPoint}/${hsmBase}/archives/.(tag)(RetentionPolicy)"
+#             )
+#             E.g., "hsm"
+#
+#   $targetSize:
+#             This is the minimum size of archives to be created.
+#             E.g. "1073741824" (1gb)
+#
+#   $packRemainingInterval:
+#             This is the interval in minutes after which all remaining files are packed into an archive
+#             regardless of its size.
+#
+#######################################################
+#
+#   prerequisites
+#
+LOG=/tmp/pack-files.log
+averageFileCount=20
+pnfsidRegex="^[ABCDEF0123456789]\{36\}$"
+
+######################################################
+#
+#   functions
+# 
+usage() {
+    echo "Usage: pack-files.sh <chimeraDataPrefix> <mountPoint> <hsmBase> <minSize> [<packRemainingInterval>]" | tee -a $LOG >&2
+}
+report() {
+    echo "`date +"%D-%T"` ($$) $1" | tee -a $LOG >&2
+}
+problem() {
+    echo "($$) $2 ($1)" | tee -a $LOG >&2
+    exit $1
+}
+errorReport() {
+    echo "($$) $1" | tee -a ${LOG} >&2
+    return 0
+}
+
+getFileSize() {
+    echo $(stat -c%s "${1}")
+}
+
+getChimeraUserFileFromFlag() {
+    cat "${mountPoint}/.(pathof)(${1})"
+}
+
+getUserFileFromFlag() {
+    cat "${mountPoint}/.(pathof)(${1})" | sed "s%${chimeraDataPrefix}%${mountPoint}%"
+}
+
+getFileSizeByPnfsId() {
+    local fileName=$(getUserFileFromFlag "${1}")
+    echo $(getFileSize "${fileName}")
+}
+
+filterDeleted() {
+    while read id
+    do
+        local answer=$(cat "${groupDir}/.(nameof)(${id})")
+        local result=$?
+        # # DEBUG
+        # report "filterDeleted: cat .(nameof)($id) -> ${answer}, ${result}"
+        [ ${result} -ne 0 ] || echo ${id}
+    done
+}
+
+filterAnswered() {
+    while read id
+    do
+        local answer=$(cat "${groupDir}/.(use)(5)(${id})")
+        local result=$?
+        # # DEBUG
+        [ ${result} -eq 0 ] && [ -z "${answer}" ] && echo ${id}
+    done
+}
+
+verifyFileExists() {
+    while read id
+    do
+        [ -f "${groupDir}/${id}" ] && echo ${id}
+    done
+}
+# reads pnfsids and collects as many as needed to create the archive
+# $1 - number of bytes to collect
+# sets the following two variables
+collectFiles() {
+    local sumSize=0
+    while read id
+    do
+        if  [ ${1} -ne 0 ]
+        then
+            [ ${sumSize} -ge ${1} ] && break
+        fi
+        sumSize=$(( ${sumSize}+$(getFileSizeByPnfsId "${id}") ))
+        echo "${id}"
+        printf '+' >&2
+    done
+    printf "\n" >&2
+    # append total size. (hack #1)
+    echo ${sumSize}
+}
+######################################################
+#
+#   traps
+#
+cleanupLock() {
+    report "    removing lock ${lockDir}"
+    rm -f "${lockDir}/$$"
+    rmdir "${lockDir}"
+}
+cleanupTmpDir() {
+    report "    cleaning up tmp directory ${tmpDir}"
+    rm -rf "${tmpDir}"
+}
+cleanupArchive() {
+    report "    deleting archive ${archiveFile}"
+    rm -f "${archiveFile}"
+}
+######################################################
+#
+#   main
+#
+# check usage
+if [ $(whoami) != "root" ]
+then
+    report "pack-files.sh must run as root."
+    exit 5
+fi
+
+if [ $# -ne 5 ]
+then 
+    usage
+    exit 4
+fi
+
+# assign parameters
+chimeraDataPrefix="${1}"
+mountPoint="${2}"
+hsmBase="${3}"
+targetSize="${4}"
+packRemainingInterval="${5}"
+
+# construct absolute requests and archives dirs
+requestsDir="${mountPoint}/${hsmBase}/requests"
+archivesDir="${mountPoint}/${hsmBase}/archives"
+# checking for existing flag directories for archivation requests
+report "Looking for archivation requests in ${requestsDir}"
+
+IFS=$'\n'
+flagDirs=($(find "${requestsDir}" -mindepth 2 -maxdepth 2 -type d))
+IFS=$' '
+dirCount=${#flagDirs[@]}
+report "  found $dirCount request groups."
+
+# iterate over all found OSMTemplate/sGroup directory combinations 
+for groupDir in ${flagDirs[@]}
+do
+    cd "${groupDir}"
+
+    lockDir="${groupDir}/.lock"
+    if ! mkdir "${lockDir}"
+    then
+      report "    leaving locked directory ${groupDir}"
+      continue
+    fi
+    trap "cleanupLock; exit 130" SIGINT SIGTERM
+
+    # create pid file in lock directory
+    touch "${lockDir}/$$"
+
+    report "    processing flag files in ${groupDir}"
+
+    # remember tags of user files for later
+    osmTemplate=$(expr "${groupDir}" : ".*/\([^/]*\)/[^/]*/[^/]*$")
+    storageGroup=$(expr "${groupDir}" : ".*/\([^/]*\)/[^/]*$")
+    dirHash=$(expr "${groupDir}" : ".*/\([^/]*\)$")
+    hsmInstance="dcache"
+    uriTemplate="${hsmInstance}://${hsmInstance}/?store=${osmTemplate}&group=${storageGroup}"
+    groupSubDir="${osmTemplate}/${storageGroup}/${dirHash}"
+    report "      using $uriTemplate for files with in group $groupSubDir"
+
+    IFS=$'\n'
+    flagFiles=($(find "${groupDir}" -name "0000*" -printf "%f\n"|grep -e "${pnfsidRegex}"|filterAnswered|verifyFileExists|filterDeleted|collectFiles "${targetSize}"))
+    IFS=$' '
+
+    # read sum of files which comes as the last element of $flagFiles (hack #1) and unset it afterwards
+    fileCount=$((${#flagFiles[@]}-1))
+    sumSize=${flagFiles[${fileCount}]}
+    unset flagFiles[${fileCount}]
+
+    # # DEBUG
+    # report "      processing the following files:"
+    # for flag in ${flagFiles[@]} ; do
+    #     report "$flag"
+    # done
+    
+    # if the combined size is not enough, 
+    if [ ${fileCount} -lt 0 ] || (( ${sumSize} < ${targetSize} ))
+    then
+        if [ ! -z ${packRemainingInterval} ]
+        then
+            recentFile=$(find ${groupDir} -type f -cmin +${packRemainingInterval}|head -n 1)
+            if [ -z ${recentFile} ]
+            then
+                report "      combined size ${sumSize} < ${targetSize} and last change more recent than ${packRemainingInterval} minutes. No archive created."
+                cleanupLock
+                report "    leaving ${groupDir}"
+                continue
+            else
+                report "      Last changed file in ${groupDir} older than ${packRemainingInterval} minutes. Packing remaining files."
+            fi
+        else # continue with next group dir
+            report "      combined size ${sumSize} < ${targetSize}. No archive created."
+            cleanupLock
+            report "    leaving ${groupDir}"
+            continue
+        fi
+    fi
+
+    # create temporary directory
+    tmpDir=$(mktemp --directory)
+    trap "cleanupLock; cleanupTmpDir; exit 130" SIGINT SIGTERM
+    report "      created temporary directory ${tmpDir}"
+
+    # initialise manifest file
+    mkdir "${tmpDir}/META-INF"
+    manifest="${tmpDir}/META-INF/MANIFEST.MF"
+    echo "Date: $(date)" > "${manifest}"
+
+    for pnfsid in ${flagFiles[@]}; do
+        filePath=$(getUserFileFromFlag "${pnfsid}")
+        fileName=$(basename "${filePath}")
+        ln -s "${filePath}" "${tmpDir}/${pnfsid}"
+
+        chimeraFilePath=$(getChimeraUserFileFromFlag "${pnfsid}")
+        echo "${pnfsid}:${chimeraFilePath}" >> "${manifest}"
+    done
+
+    echo "Total ${sumSize} bytes in ${fileCount} files" >> "${manifest}"
+    report "      archiving ${fileCount} files with a total of ${sumSize} bytes"
+
+    # create directory for the archive and then pack all files by their pnfsid-link-name in an archive
+    archivesGroupDir="${archivesDir}/${groupSubDir}"
+    report "      creating directory ${archivesGroupDir}"
+    mkdir -p "${archivesGroupDir}"
+    echo "StoreName ${osmTemplate}" > "${archivesGroupDir}/.(tag)(OSMTemplate)"
+    echo "${storageGroup}" > "${archivesGroupDir}/.(tag)(sGroup)"
+
+    # create archive
+    archiveFile=$(mktemp --dry-run --suffix=".tar" --tmpdir="${archivesGroupDir}" DARC-XXXXX)
+    trap "cleanupLock; cleanupTmpDir; cleanupArchive; exit 130" SIGINT SIGTERM
+
+    report "      packing archive ${archiveFile}"
+    cd "${tmpDir}"
+    tar chf "${archiveFile}" *
+
+    # if creating the archive failed, we stop right here
+    archivingExitCode=$?
+
+    archivedFilesCount=$(tar tf "${archiveFile}"|grep -e "${pnfsidRegex}"|wc -l)
+    report "      checking archive: Expected ${fileCount}, actual ${archivedFilesCount} files"
+
+    if [ ${archivingExitCode} -ne 0 ] || (( ${archivedFilesCount} != ${fileCount} ))
+    then 
+        cleanupLock
+        cleanupTmpDir
+        cleanupArchive
+        problem ${archivingExitCode} "Error: Creation of archive ${archiveFile} file failed. Exiting"
+    fi
+    trap "cleanupLock; cleanupTmpDir; exit 130" SIGINT SIGTERM
+
+    # if we succeeded we take the pnfsid of the just generated archive and create the replies
+    archivePnfsid=$(cat "${archivesGroupDir}/.(id)($(basename ${archiveFile}))")
+    report "      success. Stored archive ${archiveFile} with PnfsId ${archivePnfsid}."
+
+    report "      storing URIs in ${groupDir}"
+    for pnfsid in ${flagFiles[@]}; do
+        uri="${uriTemplate}&bfid=${pnfsid}:${archivePnfsid}"
+        echo "${uri}" > "${groupDir}/.(use)(5)(${pnfsid})" 2>>${LOG}
+        [ $? -eq 0 ] && printf "*" >&2 && continue || report "      URI [${uri}], could not be be written to level 5 of flag file ${pnfsid}."
+    done
+    printf "\n" >&2
+
+    cleanupLock
+    cleanupTmpDir
+    report "    leaving ${groupDir}"
+done
+report "finished."
---
+++ src/skel/usr/share/dcache/lib/hsm-internal-by-group.sh
@@ -0,0 +1,307 @@
+#!/bin/sh
+#
+# $Id: hsmcpV4.sh,v 1.1 2006-05-31 14:08:15 tigran Exp $
+#
+#set -x
+#
+#
+#    dCache configuration
+#
+#   hsm set dcache -command=<fullPathToThisScript> # e.g., /usr/share/dcache/lib/hsm-internal.sh
+#   hsm set dcache -hsmBase=<SubdirectoryBelowDataRootToTheMigratingFileSystem> # e.g., hsm
+#   hsm set dcache -dataRoot=<rootDataDir> # e.g., /data
+#
+#########################################################
+#
+#   prerequisits
+#
+LOG=/tmp/hsmio.log
+DEVTTY=$LOG
+AWK=gawk
+LIBPDCAP="/usr/lib64/libpdcap.so.1"
+DCAP_DOOR="dcache-lab000:22125"
+CHIMERA_PARAMS="org.postgresql.Driver jdbc:postgresql://dcache-lab000/chimera?prepareThreshold=3 PgSQL chimera - "
+
+#
+#
+#########################################################
+#
+#  some help functions
+#
+usage() {
+   echo "Usage : put|get <pnfsId> <filePath> [-si=<storageInfo>] [-key[=value] ...]" | tee -a $LOG >&2
+}
+report() {
+   echo "`date +"%D-%T"` ($$) $pnfsid $1" | tee -a $LOG >&2
+}
+problem() {
+   echo "($$) $2 ($1)" | tee -a $LOG >&2
+   exit $1
+}
+errorReport() {
+   echo "($$) $1" | tee -a ${LOG} >&2
+   return 0
+}
+#
+#
+#########################################################
+#
+#  Resolve pnfsID into canonical file name.
+#  Argument : pnfsID
+#  Returns  : filename
+#  expects  : pnfsMountpoint
+#
+resolvePnfsID() {
+   cpathof ${CHIMERA_PARAMS} "${1}"
+   return $?
+}
+#
+#########################################################
+#
+getStorageInfoKey() {
+
+   echo $si | 
+   $AWK -v key=$1 -F';' '{
+             for(i=1;i<=NF;i++){
+                split($i,a,"=") ;
+                if(a[1]==key)print a[2]
+             }
+   }'
+}
+#
+#     end of init
+#
+#########################################################
+#
+datasetPut() {
+   ydataRoot=${1}
+   yhsmBase=${2}
+   ystore=${3}
+   ygroup=${4}
+   ybfid=${5}
+#
+#  Generate PATH
+#
+    requestsBase="${ydataRoot}/${yhsmBase}/requests"
+    requestPath="${requestsBase}/${ystore}/${ygroup}"
+    requestFlag="${requestPath}/${ybfid}"
+#
+    report "Using request flag : ${requestFlag}"
+#
+    reply=$(creadlevel ${CHIMERA_PARAMS} "${requestFlag}" 5)
+    if [ ! -z "${reply}" ] ; then
+#
+       report "Request answer found : ${reply}"
+       iserror=`expr "${reply}" : "ERROR \([0-9]*\).*"`
+       if [ $? -eq 0 ] ; then
+          report "Found error ${iserror}"
+          crm ${CHIMERA_PARAMS} "${requestFlag}"
+          return ${iserror}
+       else 
+          crm ${CHIMERA_PARAMS} "${requestFlag}"
+          echo $reply
+          return 0
+       fi
+#
+    elif cstat ${CHIMERA_PARAMS} "${requestFlag}" > /dev/null ; then
+#
+       report "Still waiting" 
+       problem 2 "Not yet ready"
+#
+    else
+#
+       report "Initializing request" 
+       cmkdir ${CHIMERA_PARAMS} "${requestsBase}/${ystore}" 2>/dev/null
+       cmkdir ${CHIMERA_PARAMS} "${requestsBase}/${ystore}/${ygroup}" 2>/dev/null
+       ctouch ${CHIMERA_PARAMS} "${requestFlag}"
+       problem 3 "Request Initialized (async)"
+#
+    fi
+     
+    problem 102  "We should never end up here"
+}
+#
+###############################################################
+###############################################################
+###############################################################
+#
+#   The big main
+#
+###############################################################
+#
+#
+#  say hallo to people
+#
+report "$*"
+#
+###############################################################
+#
+#   have we been called correctly ?
+#
+if [ $# -lt 3 ] ; then 
+   usage
+   exit 4
+fi
+#
+#
+##################################################################################
+#
+# split the arguments into the options -<key>=<value> and the 
+# positional arguments.
+#
+report "Splitting arguments"
+args=""
+while [ $# -gt 0 ] ; do
+  if expr "$1" : "-.*" >/dev/null ; then
+     a=`expr "$1" : "-\(.*\)" 2>/dev/null`
+     key=`echo "$a" | $AWK -F= '{print $1}' 2>/dev/null`
+     value=`echo "$a" | $AWK -F= '{for(i=2;i<NF;i++)x=x $i "=" ; x=x $NF ; print x }' 2>/dev/null`
+     if [ -z "$value" ] ; then a="${key}=" ; fi
+     eval "${key}=\"${value}\""
+     a="export ${key}"
+     eval "$a"
+  else
+     args="${args} $1"
+  fi
+  shift 1
+done
+if [ ! -z "$args" ] ; then
+   set `echo "$args" | $AWK '{ for(i=1;i<=NF;i++)print $i }'`
+fi
+#
+##################################################################################
+#
+# assign the manditory arguments
+#
+command="${1}"
+pnfsid="${2}"
+filename="${3}"
+#
+###############################################################
+#
+# check for some basic variables
+#
+report "Checking hsmBase"
+#
+[ -z "${hsmBase}" ] && problem 3 "Variable 'hsmBase' not defined"
+#
+report "Checking dataRoot"
+#
+[ -z "${dataRoot}" ] && problem 3 "Variable 'dataRoot' not defined"
+#
+# make sure the storage info variables are available
+# (Will be fetched with getStorageInfoKey)
+#
+report "Checking SI"
+#
+[ -z "${si}" ] && problem 1 "StorageInfo (-si=...) not available" 
+#
+#########################################################
+#
+#   osm specific variables
+#
+store=`getStorageInfoKey store 2>/dev/null`
+group=`getStorageInfoKey group 2>/dev/null`
+bfid=`getStorageInfoKey bfid 2>/dev/null`
+#
+###############################################################
+#
+#      simulate mount time
+#
+report "Simulating mount time"
+#
+if [ -z "${waitTime}" ] ; then waitTime=0 ; fi
+#
+if [ $waitTime != 0 ]
+  then
+     report "Waiting ${waitTime} seconds"
+     sleep ${waitTime}
+     report "Returning from waiting ${waitTime} seconds"
+fi
+#
+###############################################################
+#
+if [ $command = "get" ] ; then
+   #
+   #  splitting URI into pieces
+   #
+   report "Splitting URI into pieces"
+   #
+   getStore=`expr "${uri}" : ".*/?store=\(.*\)&group.*"`
+   getGroup=`expr "${uri}" : ".*group=\(.*\)&bfid.*"`
+   getBfid=`expr "${uri}" : ".*bfid=\(.*\)"`
+   #
+   report "URI : ${uri}"
+   report "Store=${getStore}; Group=${getGroup}; Bfid=${getBfid}"
+#
+   [ \( -z "${getStore}" \) -o \( -z "${getGroup}" \) -o \( -z "${getBfid}" \) ] && \
+      problem 22 "couldn't get sufficient info for 'copy' : store=>${getStore}< group=>${getGroup}< bfid=>${getBfid}<" 
+   #
+   # Is this an archive ?
+   #
+   archiveId=`expr "${getBfid}" : ".*:\(.*\)" 2>/dev/null`
+   if [ $? -ne 0 ] ; then
+      problem 1 "This is not an archive : ${getBfid}"
+   fi
+   #
+   # find archived path
+   #
+   archiveFile=$(resolvePnfsID "${archiveId}")
+   if [ $? -ne 0 ] ; then problem 2 "Problem resolving $archiveId" ; fi
+   #
+   report "Archive file is (canonical) : ${archiveFile}"
+   #
+   originalId=`expr "${getBfid}" : "\(.*\):.*" 2>/dev/null`
+   report "Data File Pnfs ID : ${originalId}"
+   #
+   extractDir=`dirname "${filename}"`
+   #
+   report "Extracting file into $extractDir"
+   #
+   cd "${extractDir}"
+   report "Preloading ${LIBPDCAP}"
+   export LD_PRELOAD="${LIBPDCAP}"
+   tar x --force-local --file="dcap://${DCAP_DOOR}/${archiveFile}" "${originalId}" 2>>$LOG
+   rc=$?
+   cd -
+   if [ $rc -ne 0 ] ; then problem 4 "Tar couldn't replay the file" ; fi
+   #
+   report "Extraction finished, done"
+   #
+   exit 0 
+   #
+elif [ $command = "put" ] ; then
+#
+#   and the put
+#
+   filesize=$(stat "${filename}" -c%s)
+   #
+   #  check for existence of file
+   #  NOTE : if the filesize is zero, we are expected to return 31, so that
+   #         dcache can react accordingly.
+   #
+   [ -z "${filesize}" ] && problem 31 "File not found : ${filename}"
+   [ "${filesize}" = "0" ] && problem 31 "Filesize is zero (${filename})" 
+   #
+   #
+   #  now, finally copy the file to the HSM
+   #  (we assume the bfid to be returned)
+   #
+   result=`datasetPut "${dataRoot}" "${hsmBase}" "${store}" "${group}" "${pnfsid}"` || exit $?
+   # result=`sfput ${CHIMERA_PARAMS} "${dataRoot}" "${hsmBase}" "${store}" "${group}" "${pnfsid}"` || exit $?
+   #
+   # osm://osm/?store=sql&group=chimera&bfid=3434.0.994.1188400818542
+   #
+   report "Result : ${result}"
+   #
+   echo "${result}"
+   #
+   exit 0
+elif [ $command = "next" ] ; then
+   echo "'next' operation not supported by this HSM" 1>&2
+   exit 10 
+else 
+   echo "Illegal command $command" 1>&2
+   exit 4
+fi
+exit 0
