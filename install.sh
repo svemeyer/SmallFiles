67cd535d099c82b04e07bc6c062921b5a6b1da8b
Karsten Schwank <karsten.schwank@desy.de>
2014-06-18 11:55:21 +0200

    improve install script for packing node
    add creation of directories /etc/dcache and /var/log/dcache
    and copying of example config file container.conf to /etc/dcache/

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
 SRC_BIN="src/skel/usr/local/bin"
+SRC_ETC="src/skel/etc/dcache"
 SRC_LIB="src/skel/usr/share/dcache/lib"
 LOCAL_BIN="/usr/local/bin/"
+LOCAL_ETC="/etc/dcache"
+LOCAL_LOG="/var/log/dcache"
 DCACHE_LIB="/usr/share/dcache/lib/"
 
 if [ ! $# = 1 ];
 then
+  echo "This script needs root privileges"
+  echo 
   echo "Usage: install <mode>"
   echo "Modes: "
   echo "  pack: Install packing scripts"
@@ -21,6 +26,11 @@ then
   cp "${SRC_BIN}/pack-files.py" "${LOCAL_BIN}"
   cp "${SRC_BIN}/fillmetadata.py" "${LOCAL_BIN}"
   cp "${SRC_BIN}/writebfids.py" "${LOCAL_BIN}"
+  echo "Copying configuration file to ${LOCAL_ETC}"
+  mkdir -p "${LOCAL_ETC}"
+  cp "${SRC_ETC}/container.conf" "${LOCAL_ETC}"
+  echo "Creating log directory ${LOCAL_LOG}"
+  mkdir -p "${LOCAL_LOG}"
 fi
 
 if [ ${1} = "pool" ];
